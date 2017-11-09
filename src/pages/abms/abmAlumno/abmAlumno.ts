7a1dcfa1baf71368d671789ac86c7eae71742d9c
Juanma Barreda Fafasuli <juanmanuelbf@gmail.com>
2017-11-09 00:47:54 -0300

    bueno, a dormir, ma;ana se sigue

--- src/pages/abms/abmAlumno/abmAlumno.ts
+++ src/pages/abms/abmAlumno/abmAlumno.ts
@@ -36,10 +36,12 @@ export class AbmAlumnoPage {
         buttons: [
           {
             text: 'Si',
+            role: 'destructive',
             handler: data => { this.alumnos.remove(alumnoId); }
           },
           {
             text: 'No',
+            role: 'cancel',
             handler: data => { }
           }
         ]
@@ -60,100 +62,8 @@ export class AbmAlumnoPage {
     }
 
     /*
-            (ionInput)="onInput($event)"
+        (ionInput)="onInput($event)"
         (ionCancel)="onCancel($event)">
     */
   
-    /*addSong(){
-      this.songs.push({
-        title: "yo mama"
-      });
-      let prompt = this.alertCtrl.create({
-        title: 'Song Name',
-        message: "Enter a name for this new song you're so keen on adding",
-        inputs: [
-          {
-            name: 'title',
-            placeholder: 'Title'
-          },
-        ],
-        buttons: [
-          {
-            text: 'Cancel',
-            handler: data => {
-              console.log('Cancel clicked');
-            }
-          },
-          {
-            text: 'Save',
-            handler: data => {
-              this.songs.push({
-                title: data.title
-              });
-            }
-          }
-        ]
-      });
-      prompt.present();
-    }
-  
-    showOptions(songId, songTitle) {
-      let actionSheet = this.actionSheetCtrl.create({
-        title: 'What do you want to do?',
-        buttons: [
-          {
-            text: 'Delete Song',
-            role: 'destructive',
-            handler: () => {
-              this.removeSong(songId);
-            }
-          },{
-            text: 'Update title',
-            handler: () => {
-              this.updateSong(songId, songTitle);
-            }
-          },{
-            text: 'Cancel',
-            role: 'cancel',
-            handler: () => {
-              console.log('Cancel clicked');
-            }
-          }
-        ]
-      });
-      actionSheet.present();
-    }
-
-  
-    updateSong(songId, songTitle){
-      let prompt = this.alertCtrl.create({
-        title: 'Song Name',
-        message: "Update the name for this song",
-        inputs: [
-          {
-            name: 'title',
-            placeholder: 'Title',
-            value: songTitle
-          },
-        ],
-        buttons: [
-          {
-            text: 'Cancel',
-            handler: data => {
-              console.log('Cancel clicked');
-            }
-          },
-          {
-            text: 'Save',
-            handler: data => {
-              this.songs.update(songId, {
-                title: data.title
-              });
-            }
-          }
-        ]
-      });
-      prompt.present();
-    }*/
-
 }
