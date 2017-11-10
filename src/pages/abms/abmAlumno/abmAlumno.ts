4dbbbe9db479e2b39f7babac3a914426ec440473
Juanma Barreda Fafasuli <juanmanuelbf@gmail.com>
2017-11-10 02:08:04 -0300

    identado como dios manda

--- src/pages/abms/abmAlumno/abmAlumno.ts
+++ src/pages/abms/abmAlumno/abmAlumno.ts
@@ -25,70 +25,67 @@ export class AbmAlumnoPage {
     public af: AngularFireDatabase,
     public actionSheetCtrl: ActionSheetController, 
     private formBuilder: FormBuilder) {
-        this.tab = "lista";
-        //Lista
-        this.filterType = "Apellido";
-        //Alta
-        this.filterAlumno();
-        this.formAlta = this.formBuilder.group({
-          nombre: ['', Validators.compose([Validators.required, Validators.minLength(3)])],
-          apellido: ['', Validators.compose([Validators.required, Validators.minLength(3)])],
-          legajo: ['', Validators.compose([Validators.required, Validators.minLength(4), Validators.maxLength(4), Validators.pattern("[-+]?[0-9]*\.?[0-9]*")])],
-          anio: ['', Validators.compose([Validators.required])],
-          curso: ['', Validators.compose([Validators.required])],
-        });
-    }
-
-    //LISTA DE ALUMNOS
-public eliminarAlumno(alumnoId: string, apellido: string): void {
-  let prompt = this.alertCtrl.create({
-    title: 'Confirmar',
-        message: "Seguro que queres eliminar al alumno " + apellido + "?",
-        buttons: [
-          {
-            text: 'Si',
-            role: 'destructive',
-            handler: data => { this.alumnos.remove(alumnoId); }
-          },
-          {
-            text: 'No',
-            role: 'cancel',
-            handler: data => { }
-          }
-        ]
-      });
-      prompt.present();
-    }
-
-    modificarAlumno(alumnoId: string, apellido: string): void {
-      
-    }
-
-    //AGREGAR ALUMNO
-    public agregarAlumno(): void{
-      let prompt = this.alertCtrl.create({ title: 'Alumno agregado', buttons: [{ text: 'Ok',}] });
-      prompt.present();
-      this.af.list('/alumnos').push(this.formAlta.value);
-      this.formAlta.reset();
-    }
-
-    public onInput($event): void {
+      this.tab = "lista";
+      //Lista
+      this.filterType = "Apellido";
+      //Alta
       this.filterAlumno();
-    }
+      this.formAlta = this.formBuilder.group({
+        nombre: ['', Validators.compose([Validators.required, Validators.minLength(3)])],
+        apellido: ['', Validators.compose([Validators.required, Validators.minLength(3)])],
+        legajo: ['', Validators.compose([Validators.required, Validators.minLength(4), Validators.maxLength(4), Validators.pattern("[-+]?[0-9]*\.?[0-9]*")])],
+        anio: ['', Validators.compose([Validators.required])],
+        curso: ['', Validators.compose([Validators.required])],
+      });
+  }
 
-    private filterAlumno(): boolean {
-      this.alumnos = this.af.list('/alumnos').map(alumno => alumno.filter(alumno => {
-        if(this.searchValue != "" && this.searchValue != undefined) {
-          if(this.filterType == "Nombre"){
-            return alumno.nombre.indexOf(this.searchValue) > 0;
-          } else if(this.filterType == "Apellido"){
-            return alumno.apellido.indexOf(this.searchValue) > 0;
-          } else if(this.filterType == "Legajo"){
-            return alumno.legajo.indexOf(this.searchValue) > 0;
-          }
+  //LISTA DE ALUMNOS
+  public eliminarAlumno(alumnoId: string, apellido: string): void {
+    let prompt = this.alertCtrl.create({
+      title: 'Confirmar',
+      message: "Seguro que queres eliminar al alumno " + apellido + "?",
+      buttons: [{
+        text: 'Si',
+        role: 'destructive',
+        handler: data => { this.alumnos.remove(alumnoId); }
+      },
+      {
+        text: 'No',
+        role: 'cancel',
+        handler: data => { }
+      }]
+    });
+    prompt.present();
+  }
+
+  public modificarAlumno(alumnoId: string, apellido: string): void {
+      
+  }
+
+  //AGREGAR ALUMNO
+  public agregarAlumno(): void{
+    let prompt = this.alertCtrl.create({ title: 'Alumno agregado', buttons: [{ text: 'Ok',}] });
+    prompt.present();
+    this.af.list('/alumnos').push(this.formAlta.value);
+    this.formAlta.reset();
+  }
+
+  public onInput($event): void {
+    this.filterAlumno();
+  }
+
+  private filterAlumno(): boolean {
+    this.alumnos = this.af.list('/alumnos').map(alumno => alumno.filter(alumno => {
+      if(this.searchValue != "" && this.searchValue != undefined) {
+        if(this.filterType == "Nombre"){
+          return alumno.nombre.indexOf(this.searchValue) > 0;
+        } else if(this.filterType == "Apellido"){
+          return alumno.apellido.indexOf(this.searchValue) > 0;
+        } else if(this.filterType == "Legajo"){
+          return alumno.legajo.indexOf(this.searchValue) > 0;
         }
-        return true;
-      }));
-    }
-
+      }
+      return true;
+    }));
+  }
 }
