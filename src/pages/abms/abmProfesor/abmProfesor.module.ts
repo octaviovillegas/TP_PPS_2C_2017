ed966b34fa027a113b8a53b8f5cbfb89aea1f1c0
maurozampini <mauro.zampini@hotmail.com>
2017-11-11 09:54:47 -0300

    ABM Profesor

---
+++ src/pages/abms/abmProfesor/abmProfesor.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { IonicPageModule } from 'ionic-angular';
+import { AbmProfesorPage } from './abmProfesor';
+
+@NgModule({
+  declarations: [
+    AbmProfesorPage,
+  ],
+  imports: [
+    IonicPageModule.forChild(AbmProfesorPage),
+  ],
+})
+export class AbmProfesorPageModule {}
