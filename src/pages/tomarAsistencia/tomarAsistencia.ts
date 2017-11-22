25f51852c20e3019d536bd0c15b4a3daca78f0ce
Juanma Barreda Fafasuli <juanmanuelbf@gmail.com>
2017-11-22 00:04:55 -0300

    la lista se crea dinamicamente

--- src/pages/tomarAsistencia/tomarAsistencia.ts
+++ src/pages/tomarAsistencia/tomarAsistencia.ts
@@ -8,9 +8,40 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class TomarAsistenciaPage {
 
+  public anios: Array<number> = [1, 2, 3, 4, 5];
+  public cursos: Array<string> = ['A', 'B'];
   public estado;
 
   constructor(public navCtrl: NavController, public navParams: NavParams) {
-    this.estado == "cursos";
+    this.estado = "cursos";
   }
+
+  public mostrarCurso(anio: number, curso: string): void {
+    console.log(anio);
+    console.log(curso);
+  }
+
+  public estaCompleto(anio: number, curso: string): boolean {
+    if(anio < 3) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  public getTitulo(anio: number) {
+    switch(anio){
+      case 1:
+        return "Primero";
+      case 2:
+        return "Segundo";
+      case 3:
+        return "Tercero";
+      case 4:
+        return "Cuarto";
+      case 5:
+        return "Quinto";
+    }
+  }
+
 }
