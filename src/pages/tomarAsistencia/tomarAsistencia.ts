a9cae94ffcb846c4610ae50adfd9c9f06bfaa9dc
Juanma Barreda Fafasuli <juanmanuelbf@gmail.com>
2017-11-22 03:20:38 -0300

    tomaaaaa ANGULAR PARA VOS, MIRA DE QUIEN TE REISTE

--- src/pages/tomarAsistencia/tomarAsistencia.ts
+++ src/pages/tomarAsistencia/tomarAsistencia.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { AngularFireDatabase, FirebaseListObservable } from 'angularfire2/database';
 import { Observable } from 'rxjs/Observable';
+import { BehaviorSubject } from 'rxjs/BehaviorSubject';
 
 @IonicPage()
 @Component({
@@ -10,27 +11,19 @@ import { Observable } from 'rxjs/Observable';
 })
 export class TomarAsistenciaPage {
 
-  public cursos: Observable<any>;
   public estado;
+  public cursos: Observable<any>;
 
   constructor(public navCtrl: NavController, 
     public navParams: NavParams,
     public af: AngularFireDatabase) {
+      this.estado = "lista";
     this.cursos = this.af.list("/cursos");
-    console.log(this.cursos);
-    this.estado = "cursos";
   }
 
   public mostrarCurso(anio: number, curso: string): void {
     console.log(anio);
     console.log(curso);
-    this.cursoCompleto("Fasd", "fads");
-  }
-
-  public cursoCompleto(anio: string, curso: string): boolean {
-    let resultado = false;
-    console.log(resultado);
-    return resultado;
   }
 
   public getTitulo(anio: number) {
@@ -45,6 +38,14 @@ export class TomarAsistenciaPage {
         return "Cuarto";
       case 5:
         return "Quinto";
+    }
+  }
+
+  public selecAlumno(key: string) {
+    if(document.getElementById('aaa').style.visibility == "hidden"){
+      document.getElementById('aaa').style.visibility = "visible";
+    } else {
+      document.getElementById('aaa').style.visibility = "hidden";
     }
   }
 
