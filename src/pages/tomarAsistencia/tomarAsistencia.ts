adc361f3046693d4fe039871d3eefd7d4a1776f4
Juanma Barreda Fafasuli <juanmanuelbf@gmail.com>
2017-11-22 22:15:58 -0300

    fad

--- src/pages/tomarAsistencia/tomarAsistencia.ts
+++ src/pages/tomarAsistencia/tomarAsistencia.ts
@@ -14,17 +14,13 @@ export class TomarAsistenciaPage {
   public estado;
   public cursos: Observable<any>;
   public alumnos: Observable<any>;
+  public current: {} = {anio: null, curso: null};
 
   constructor(public navCtrl: NavController, 
     public navParams: NavParams,
     public af: AngularFireDatabase) {
-      this.estado = "lista";
+      this.estado = "cursos";
     this.cursos = this.af.list("/cursos");
-    this.alumnos = this.af.list("/usuarios").map(u => u.filter(u => u.tipo == "alumno"));
-  }
-
-  public mostrarCurso(anio: number, curso: string): void {
-    
   }
 
   public getTitulo(anio: number) {
@@ -40,6 +36,25 @@ export class TomarAsistenciaPage {
       case 5:
         return "Quinto";
     }
+  }
+
+  public mostrarLista(anio: string, curso: string): void {
+    console.log(anio);
+    console.log(curso);
+    this.filtrarAlumnos(anio, curso);
+    this.current.anio = anio;
+    this.current.curso = curso;
+    this.estado = "lista";
+  }
+
+  private filtrarAlumnos(anio?: string, curso?: string): void {
+    this.alumnos = this.af.list("/usuarios").map(u => 
+      u.filter(u => u.tipo == "alumno").filter(u => u.anio == anio && u.curso == curso);
+    );
+  }
+
+  public backToCursos(): void {
+    this.estado = "cursos";
   }
 
   public selecAlumno(key: string) {
