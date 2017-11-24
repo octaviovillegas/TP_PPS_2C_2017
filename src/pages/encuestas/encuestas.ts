688e2c2f98d45c3ad1cf712b9402659275204ea5
Juanma Barreda Fafasuli <juanmanuelbf@gmail.com>
2017-11-24 14:54:39 -0300

    aja

--- src/pages/encuestas/encuestas.ts
+++ src/pages/encuestas/encuestas.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { Validators, FormBuilder, FormGroup } from '@angular/forms';
+import { Chart } from 'chart.js';
 
 @IonicPage()
 @Component({
@@ -8,11 +10,21 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class EncuestasPage {
 
-  public tab: string;
+  public tab: string= "actual";
   //Actual
   public respuesta: string;
+  //Crear
+  private formCrear: FormGroup;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(public navCtrl: NavController, 
+    public navParams: NavParams,
+    private formBuilder: FormBuilder) {
+    this.formCrear = this.formBuilder.group({
+        pregunta: ['', Validators.compose([Validators.required, Validators.minLength(5)])],
+        respuestaA: ['', Validators.compose([Validators.required, Validators.minLength(1)])],
+        respuestaB: ['', Validators.compose([Validators.required, Validators.minLength(1)])],
+        tiempo: ['', Validators.compose([Validators.required])]
+      });
   }
   
 }
