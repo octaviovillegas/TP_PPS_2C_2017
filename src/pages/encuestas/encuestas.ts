adba7a14f49cdcbf8572ff3789463b49bbec7596
Juanma Barreda Fafasuli <juanmanuelbf@gmail.com>
2017-11-24 16:41:17 -0300

    dale k va

--- src/pages/encuestas/encuestas.ts
+++ src/pages/encuestas/encuestas.ts
@@ -1,7 +1,8 @@
 import { Component, ViewChild } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { IonicPage, NavController, NavParams, AlertController } from 'ionic-angular';
 import { Validators, FormBuilder, FormGroup } from '@angular/forms';
 import { Chart } from 'chart.js';
+import { AngularFireDatabase } from 'angularfire2/database';
 
 @IonicPage()
 @Component({
@@ -11,6 +12,7 @@ import { Chart } from 'chart.js';
 export class EncuestasPage {
 
   public tab: string= "estadisticas";
+  public refEncuesta = this.af.database.ref("/encuesta/");
   //Actual
   public respuesta: string;
   //Crear
@@ -22,35 +24,59 @@ export class EncuestasPage {
 
   constructor(public navCtrl: NavController, 
     public navParams: NavParams,
-    private formBuilder: FormBuilder) {
+    private formBuilder: FormBuilder,
+    public alertCtrl: AlertController, 
+    public af: AngularFireDatabase) {
     this.formCrear = this.formBuilder.group({
         pregunta: ['', Validators.compose([Validators.required, Validators.minLength(5)])],
         respuestaA: ['', Validators.compose([Validators.required, Validators.minLength(1)])],
         respuestaB: ['', Validators.compose([Validators.required, Validators.minLength(1)])],
         tiempo: ['', Validators.compose([Validators.required])]
-      });
+    });
   }
 
+  public crearEncuesta(){
+    let prompt = this.alertCtrl.create({ title: 'Encuesta creada', buttons: [{ text: 'Ok',}] });
+    prompt.present();
+    let data: {} = this.formCrear.value;
+    data["creado"] = new Date().getTime();
+    let tiempo = 0;
+    if(data["tiempo"] == "2h"){
+      tiempo = 7200000;
+    } else if (data["tiempo"] == "1h") {
+      tiempo = 3600000;
+    } else if (data["tiempo"] == "30s") {
+      tiempo = 30000;
+    }
+    data["termina"] = data["creado"] + tiempo;
+    data["terminado"] = "0";
+    this.af.list("/usuarios").push(data);
+  }
+
+  private milisegundosATiempo(duration: number): string {
+    let horNum =   (duration/(1000*60*60))%24;
+    let minNum = (duration/(1000*60))%60;
+    let segNum = (duration/1000)%60;
+    let horas = (horNum < 10) ? "0" + horNum : horNum;
+    let minutos = (minNum < 10) ? "0" + minNum : minNum;
+    let segundos = (segNum < 10) ? "0" + segNum : segNum;
+    return horas + ":" + minutos + ":" + segundos;
+ }
+
   public ionViewDidLoad() {
-    
-           this.tortaGrafico = new Chart(this.tortaCanvas.nativeElement, {
-    
-               type: 'pie',
-               data: {
-                   labels: ["Red", "Blue"],
-                   datasets: [{
-                       label: '# of Votes',
-                       data: [12, 19],
-                       backgroundColor: [
-                           '#FF6384',
-                           '#36A2EB',
-                       ],
-                       hoverBackgroundColor: [
-                           "#FF6384",
-                           "#36A2EB",
-                       ]
-                   }]
-               }
-           });
-       }
+    this.tortaGrafico = new Chart(this.tortaCanvas.nativeElement, {
+      type: 'pie',
+      data: {
+        labels: ["Red", "Blue"],
+        datasets: [{
+          label: '# of Votes',
+          data: [12, 19],
+          backgroundColor: ['#FF6384','#36A2EB',],
+          hoverBackgroundColor: ["#FF6384", "#36A2EB"]
+        }]
+      }
+    });
+  }
+
+
 }
