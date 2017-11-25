a8d46c9a66e653e1e5e0c91eddbc4e6062163b76
maurozampini <mauro.zampini@hotmail.com>
2017-11-24 23:34:50 -0300

    Se agregó sonido

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -3,6 +3,7 @@ import { NavController, AlertController } from 'ionic-angular';
 import { PagesService, PageType } from '../../services/pages.service';
 import { AngularFireAuth } from 'angularfire2/auth';
 import { LoginPage } from '../login/login';
+import { NativeAudio } from '@ionic-native/native-audio';
 //import { SocketService } from "../../services/socket.service";
 
 @Component({
@@ -17,6 +18,7 @@ export class HomePage {
   constructor(public navCtrl: NavController,
     public authAf: AngularFireAuth,
     private alertCtrl: AlertController,
+    private nativeAudio: NativeAudio,
 	  /*private socketService: SocketService*/) {    var pagesService = new PagesService();
     this.pages = pagesService.getByUserType(this.user);
   }
@@ -31,6 +33,7 @@ export class HomePage {
 
   public cerrarSesion(): void
   {
+    this.nativeAudio.play('assets/sounds/sonido.mp3');
     let alert = this.alertCtrl.create({
       title: 'Cerrar sesión',
       message: '¿Desea cerrar la sesión?',
