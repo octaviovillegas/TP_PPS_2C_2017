0fa4daeeb49f867c5c4eb320a786559bfcbfeadb
Juanma Barreda Fafasuli <juanmanuelbf@gmail.com>
2017-12-09 11:10:42 -0300

    Basico

---
+++ src/pages/qr/qr-profesores/qr-profesores.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+import { IonicPage, NavController } from 'ionic-angular';
+
+@IonicPage()
+@Component({
+  selector: 'page-qr-profesores',
+  templateUrl: 'qr-profesores.html',
+})
+export class QrProfesoresPage {
+
+    constructor(public navCtrl: NavController) 
+    {
+        
+    }
+    
+}
